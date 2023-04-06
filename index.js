c5d60c6c6fdfe25470e933d444ab444d61cea843
Fortunechinenyem <fortunechinenyem@gmail.com>
2023-04-06 11:31:17 +0100

    update

--- index.js
+++ index.js
@@ -11,4 +11,18 @@ navLinks.forEach((link) => {
   });
 });
 
+var header = document.querySelector("h1");
 
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function changeHeaderColor() {
+  header.style.color = getRandomColor();
+}
+setInterval("changeHeaderColor()", 500);
